3a47db24feac6179165645a3c9fc85c304598964
RubenKelevra <ruben@freifunk-nrw.de>
2015-03-11 18:51:29 +0100

    append filewrite to stat-call

--- files/usr/lib/libfnetstat/lib-print.sh
+++ files/usr/lib/libfnetstat/lib-print.sh
@@ -2,21 +2,38 @@
 # RubenKelevra 2012-2015 - <ruben@freifunk-nrw.de>
 # Lizenz: AGPL 3.0
 
+_print_filename=""
+
+function set_print_to_file() {
+    _print_filename="$1"
+}
+
+function _check_print_file() {
+    if [ -z "$_print_filename" ]; then
+        exit 1
+    fi
+}
+
 function print_stat_header() {
-    json_open
-    json_add_str "sys:script" "v0.6b"
-    json_add_str "type"       "stat"
+    _check_print_file
+    
+    json_open > "$_print_filename"
+    json_add_str "sys:script" "v0.6b" >> "$_print_filename"
+    json_add_str "type"       "stat" >> "$_print_filename"
 }
 
 function print_info_header() {
-    json_open
-    json_add_str "sys:script" "v0.6b"
-    json_add_str "type"       "info"
+    _check_print_file
+
+    json_open > "$_print_filename"
+    json_add_str "sys:script" "v0.6b" >> "$_print_filename"
+    json_add_str "type"       "info" >> "$_print_filename"
 }
 
 function print_hostname() {
     if [ "$1" == "as json" ]; then
-        json_add_str "sys:hostname" "$(get_hostname)"
+        _check_print_file
+        json_add_str "sys:hostname" "$(get_hostname)" >> "$_print_filename"
     else
         get_hostname
     fi
@@ -25,82 +42,94 @@ function print_hostname() {
 fuction print_bootid() {
     checkBootID
     if [ "$1" == "as json" ]; then
-        json_add_str "sys:boot_id" "$_BootID"
+        _check_print_file
+        json_add_str "sys:boot_id" "$_BootID" >> "$_print_filename"
     else
         echo "$_BootID"
     fi
 }
 function print_wan_ip4() {
     if [ "$1" == "as json" ]; then
-        json_add_str "net:wan:ip4" "$(get_wan_ip4)"
+        _check_print_file
+        json_add_str "net:wan:ip4" "$(get_wan_ip4)" >> "$_print_filename"
     else
         get_wan_ip4
     fi
 }
 function print_wan_ip6() {
     if [ "$1" == "as json" ]; then
-        json_add_str "net:wan:ip6" "$(get_wan_ip6)"
+        _check_print_file
+        json_add_str "net:wan:ip6" "$(get_wan_ip6)" >> "$_print_filename"
     else
         get_wan_ip6
     fi
 }
 function print_pingcheck_wan_ip4() {
     if [ "$1" == "as json" ]; then
-        json_add_dec "net:wan:ip4:ping" "$(pingcheck_wan_ip4)"
+        _check_print_file
+        json_add_dec "net:wan:ip4:ping" "$(pingcheck_wan_ip4)" >> "$_print_filename"
     else
         pingcheck_wan_ip4
     fi
 }
 function print_pingcheck_wan_ip6() {
     if [ "$1" == "as json" ]; then
-        json_add_dec "net:wan:ip6:ping" "$(pingcheck_wan_ip6)"
+        _check_print_file
+        json_add_dec "net:wan:ip6:ping" "$(pingcheck_wan_ip6)" >> "$_print_filename"
     else
         pingcheck_wan_ip6
     fi
 }
 function print_uptime() {
     if [ "$1" == "as json" ]; then
-        json_add_dec "sys:uptime" "$(get_uptime)"
+        _check_print_file
+        json_add_dec "sys:uptime" "$(get_uptime)" >> "$_print_filename"
     else
         get_uptime
     fi
 }
 function print_idletime() {
     if [ "$1" == "as json" ]; then
-        json_add_dec "sys:idletime" "$(get_idletime)"
+        _check_print_file
+        json_add_dec "sys:idletime" "$(get_idletime)" >> "$_print_filename"
     else
         get_idletime
     fi
 }
 function print_idletime() {
     if [ "$1" == "as json" ]; then
-        json_add_str "sys:localtime" "$(get_time)"
+        _check_print_file
+        json_add_str "sys:localtime" "$(get_time)" >> "$_print_filename"
     else
         get_time
     fi
 }
 function print_meminfo() {
     if [ "$1" == "as json" ]; then
-        json_array_open "sys:memory"
-        json_add_dec "total"     "$(get_memory_total)"
-        json_add_dec "caching"   "$(get_memory_cached)"
-        json_add_dec "buffering" "$(get_memory_buffers)"
-        json_add_dec "free"      "$(get_memory_free)"
-        json_array_close
+        _check_print_file
+        
+        json_array_open "sys:memory" >> "$_print_filename"
+        json_add_dec "total"     "$(get_memory_total)" >> "$_print_filename"
+        json_add_dec "caching"   "$(get_memory_cached)" >> "$_print_filename"
+        json_add_dec "buffering" "$(get_memory_buffers)" >> "$_print_filename"
+        json_add_dec "free"      "$(get_memory_free)" >> "$_print_filename"
+        json_array_close >> "$_print_filename"
     else
         echo "$(get_memory_total), $(get_memory_cached), $(get_memory_buffers), $(get_memory_free)"
     fi
 }
 function print_processes_running() {
     if [ "$1" == "as json" ]; then
-        json_add_dec "sys:processes_running" "$(get_running_processes)"
+        _check_print_file
+        json_add_dec "sys:processes_running" "$(get_running_processes)" >> "$_print_filename"
     else
         get_running_processes
     fi
 }
 function print_loadavg() {
     if [ "$1" == "as json" ]; then
-        json_add_dec "sys:loadavg" "$(get_loadavg)"
+        _check_print_file
+        json_add_dec "sys:loadavg" "$(get_loadavg)" >> "$_print_filename"
     else
         get_loadavg
     fi
@@ -109,7 +138,8 @@ function print_loadavg() {
 function print_cpuinfo() {
     
     if [ "$1" == "as json" ]; then
-        json_list_open "sys:cpu"
+        _check_print_file
+        json_list_open "sys:cpu" >> "$_print_filename"
         for cpu_line in gen_cpustats; do
             
             
